9b0f2c2ab8b6eca24fdfb0403fcfa5e7839c823f
eseiler <emilienseiler18@gmail.com>
2024-03-19 15:01:20 +0900

    clean git

---
+++ src/src/load_save.py
@@ -0,0 +1,67 @@
+import json
+import torch
+import numpy as np
+from src.model import BuildNetwork_previous, BuildNetworkNew
+import os
+from pathlib import Path
+current_path = Path.cwd().parent.parent
+path = os.path.join(current_path, "model_history")
+
+
+def save_model(trained_model, formatted_datetime_int, equation_name, name,
+               x_range, iterations, hid_lay, num_equations, num_heads, A_list,
+               IC_list, force_list, alpha_list, loss_hist):
+    # Save model history
+    path_equation = os.path.join(path, equation_name)
+    torch.save(trained_model.state_dict(), os.path.join(path_equation, f"{name}_{formatted_datetime_int}"))
+    history = {}
+    history["x_range"] = x_range
+    history["iterations"] = iterations
+    history["hid_lay"] = [int(i) for i in hid_lay]
+    history["num_equations"] = num_equations
+    history["num_heads"] = num_heads
+
+    history["A"] = [A.cpu().numpy().tolist() for A in A_list]
+    history["v"] = [v.cpu().numpy().tolist() for v in IC_list]
+    if callable(force_list[0]):
+        history["force"] = None
+    else:
+        history["force"] = [f.cpu().numpy().tolist() for f in force_list]
+    history["alpha_list"] = alpha_list if isinstance(alpha_list, list) else alpha_list.tolist()
+
+    loss_hist["head"] = loss_hist["head"] if isinstance(loss_hist["head"], list) else loss_hist["head"].tolist()
+    history["loss_hist"] = loss_hist
+
+    with open(os.path.join(path_equation, f"history_{name}_{formatted_datetime_int}.json"), "w") as fp:
+        print(os.path.join(path_equation, f"history_{name}_{formatted_datetime_int}.json"))
+        json.dump(history, fp)
+
+
+def load_run_history(equation_name, model_file, device, prev):
+    path_equation = os.path.join(path, f"{equation_name}")
+    with open(os.path.join(path_equation, "history_" + str(model_file) + ".json")) as f:
+        history = json.load(f)
+        x_range = history["x_range"]
+        iterations = history["iterations"]
+        hid_lay = history["hid_lay"]
+        num_equations = history["num_equations"]
+        num_heads = history["num_heads"]
+
+        loss_hist = history["loss_hist"]
+        loss_hist["head"] = np.array(loss_hist["head"])
+
+        alpha_list = history["alpha_list"]
+        A_list = [torch.from_numpy(np.array(A)).to(device).double() for A in history["A"]]
+        IC_list = [torch.from_numpy(np.array(v)).to(device).double() for v in history["v"]]
+        if history["force"] is None:
+            force = None
+            print("Force change with time")
+        else:
+            force = torch.from_numpy(np.array(history["force"])).to(device).double()
+        if prev:
+            trained_model = BuildNetwork_previous(1, hid_lay[0], hid_lay[1], hid_lay[2], num_equations, num_heads).to(device, dtype=torch.double)
+        else:
+            trained_model = BuildNetworkNew(1, hid_lay, num_equations, num_heads, device, activation="tanh", IC_list=IC_list).to(device, dtype=torch.double)
+        trained_model.load_state_dict(torch.load(os.path.join(path_equation, str(model_file))))
+        trained_model.eval()
+    return trained_model, x_range, iterations, hid_lay, num_equations, num_heads, loss_hist, alpha_list, A_list, IC_list, force
---
+++ src/src/model.py
@@ -0,0 +1,166 @@
+"""Deep learning model for PINNS"""
+
+import torch.nn as nn
+import torch
+
+
+class BuildNetwork(nn.Module):
+    def __init__(self, input_size, h_sizes, output_size, n_heads, dev,
+                 activation="tanh", IC_list=None):
+        super(BuildNetwork, self).__init__()
+
+        # strore variable for reparametrization
+        self.IC_list = IC_list
+        self.dev = dev
+        # store the number of "heads" to use in the model
+        self.n_heads = n_heads
+
+        # build activation function
+        if activation == "tanh":
+            self.activation = nn.Tanh()
+        elif activation == "cos":
+            self.activation = torch.cos()
+        elif activation == "relu":
+            self.activation = nn.ReLU()
+        elif activation == "elu":
+            self.activation = nn.ELU()
+        else:
+            raise ValueError("Invalid activation function. Use 'tanh'.")
+
+        # build the layers to use for the forward pass
+        self.hidden_layers = nn.ModuleList([nn.Linear(in_features, out_features, bias=True)
+                                           for in_features, out_features in zip([input_size] + h_sizes[:-1], h_sizes)])
+
+        # build n_heads output layers, each corresponding to different conditions during training
+        self.multi_head_output = nn.ModuleList(
+            [nn.Linear(h_sizes[-1], output_size)])
+        self.multi_head_output.extend(
+            [nn.Linear(h_sizes[-1], output_size) for i in range(n_heads - 1)])
+
+    def forward(self, x, reparametrization=False):
+        # dictionary to store the output for each "head" in the model
+        u_results = {}
+
+        # all "heads" have the same pass through the hidden laers
+        result = x
+        for layer in self.hidden_layers:
+            result = self.activation(layer(result))
+        h = result
+
+        # apply the corresponding output layer to each "head"
+        for i in range(self.n_heads):
+            result_i = self.multi_head_output[i](h)
+            if reparametrization:
+                if isinstance(self.IC_list, list) and all(isinstance(item, torch.Tensor) for item in self.IC_list):
+                    u0 = self.IC_list[i]
+                elif isinstance(self.IC_list, torch.Tensor):
+                    u0 = self.IC_list
+                else:
+                    raise ValueError(f"Need IC_list as a list os Tensor for reparametrization\n Here IC_list={self.IC_list}")
+                N0 = self.forward(torch.tensor([[0]], dtype=torch.float32, device=self.dev))[0][f"head {i + 1}"]
+                result_i = result_i + ((u0.T - N0).expand(x.shape[0], -1) * torch.exp(-x))
+            u_results[f"head {i + 1}"] = result_i
+
+        return u_results, h
+
+
+class BuildNetworkNew(nn.Module):
+    def __init__(self, input_size, h_sizes, output_size, n_heads, dev,
+                 activation="tanh", IC_list=None):
+        super(BuildNetworkNew, self).__init__()
+
+        # strore variable for reparametrization
+        self.IC_list = IC_list
+        self.dev = dev
+        # store the number of "heads" to use in the model
+        self.n_heads = n_heads
+
+        # build activation function
+        if activation == "tanh":
+            self.activation = nn.Tanh()
+        elif activation == "cos":
+            self.activation = torch.cos()
+        elif activation == "relu":
+            self.activation = nn.ReLU()
+        elif activation == "elu":
+            self.activation = nn.ELU()
+        elif activation == "celu":
+            self.activation = nn.CELU()
+        elif activation == "silu":
+            self.activation = nn.SiLU()
+        else:
+            raise ValueError("Invalid activation function")
+
+        # build the layers to use for the forward pass
+        self.hidden_layers = nn.ModuleList([nn.Linear(in_features, out_features, bias=True)
+                                           for in_features, out_features in zip([input_size] + h_sizes[:-1], h_sizes)])
+
+        # build n_heads output layers, each corresponding to different conditions during training
+        self.multi_head_output = nn.ModuleList(
+            [nn.Linear(h_sizes[-1], output_size)])
+        self.multi_head_output.extend(
+            [nn.Linear(h_sizes[-1], output_size) for i in range(n_heads - 1)])
+
+    def forward(self, x, reparametrization=False):
+
+        # all "heads" have the same pass through the hidden laers
+        result = x
+        for layer in self.hidden_layers:
+            result = self.activation(layer(result))
+        h = result
+
+        # apply the corresponding output layer to each "head"
+        output = []
+        for i in range(self.n_heads):
+            result_i = self.multi_head_output[i](h)
+            if reparametrization:
+                if isinstance(self.IC_list, list) and all(isinstance(item, torch.Tensor) for item in self.IC_list):
+                    u0 = self.IC_list[i]
+                elif isinstance(self.IC_list, torch.Tensor):
+                    u0 = self.IC_list
+                else:
+                    raise ValueError(f"Need IC_list as a list os Tensor for reparametrization\n Here IC_list={self.IC_list}")
+                N0 = self.forward(torch.tensor([[0]], dtype=torch.float64, device=self.dev))[0][:, i, :]
+                result_i = result_i + ((u0.T - N0).expand(x.shape[0], -1) * torch.exp(-x))
+            output.append(result_i)
+
+        return torch.stack(output, dim=1), h
+
+
+# class to build the network
+class BuildNetwork_previous(nn.Module):
+    def __init__(self, input_size, h_size1, h_size2, h_size3, output_size, n_heads):
+        super(BuildNetwork_previous, self).__init__()
+        # store the number of "heads" to use in the model
+        self.n_heads = n_heads
+
+        # build the layers to use for the forward pass
+        self.l1 = nn.Linear(input_size, h_size1)
+        self.tanh = nn.Tanh()
+        self.l2 = nn.Linear(h_size1, h_size2)
+        self.l3 = nn.Linear(h_size2, h_size3)
+
+        # build n_heads output layers, each corresponding to different conditions during training
+        self.multi_head_output = nn.ModuleList(
+            [nn.Linear(h_size3, output_size)])
+        self.multi_head_output.extend(
+            [nn.Linear(h_size3, output_size) for i in range(n_heads - 1)])
+
+    def forward(self, x):
+        # dictionary to store the output for each "head" in the model
+        u_results = {}
+
+        # all "heads" have the same pass through the hidden laers
+        result = self.l1(x)
+        result = self.tanh(result)
+        result = self.l2(result)
+        result = self.tanh(result)
+        result = self.l3(result)
+        h = self.tanh(result)
+
+        # apply the corresponding output layer to each "head"
+        for i in range(self.n_heads):
+            result_i = self.multi_head_output[i](h)
+            u_results[f"head {i + 1}"] = result_i
+
+        return u_results, h
